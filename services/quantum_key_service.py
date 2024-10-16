90b0b1c9719b8774813663d174b55ecd9e968f45
Nyx <128867964+Lxsanto@users.noreply.github.com>
2024-10-16 10:17:56 +0200

    Update quantum_key_service.py

--- services/quantum_key_service.py
+++ services/quantum_key_service.py
@@ -2,17 +2,16 @@ import requests
 
 class QuantumKeyService:
     def generate_qrng_key(self, length=32):
-        num_values = length  
-        api_url = f'https://qrng.anu.edu.au/API/jsonI.php?length=10&type=uint8'
+        api_length = min(length, 1024)  # Max length per request is 1024
+        api_url = f'https://qrng.anu.edu.au/API/jsonI.php?length={api_length}&type=uint8'
 
         try:
             response = requests.get(api_url)
             if response.status_code == 200:
                 data = response.json()
                 if data['success']:
-                    hex_numbers = data['data']
-                    hex_string = ''.join(hex_numbers)
-                    key_bytes = bytes.fromhex(hex_string)[:length]
+                    random_numbers = data['data']
+                    key_bytes = bytes(random_numbers)[:length]
                     return key_bytes
                 else:
                     print('ANU QRNG API error:', data.get('message', 'Unknown error'))
@@ -21,5 +20,5 @@ class QuantumKeyService:
                 print(f'HTTP Error: {response.status_code}')
                 return None
         except Exception as e:
-            print(f'Errore nella generazione della chiave quantistica: {e}')
+            print(f'Error in generating quantum key: {e}')
             return None
